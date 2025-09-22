9b1f0f41769d752979c5693147d0b9c8b834bd2a
Renan Reis Martins de Paula <rreis@abaqus.cl>
2025-09-22 16:08:43 -0300

    python init

---
+++ python/tests/test_statement.py
@@ -0,0 +1,10 @@
+import unittest
+import statements
+
+class TestStatement(unittest.TestCase):
+    def test_asset_must_have_positive_quantity(self):
+        """
+        Test statement
+        """
+        
+        self.assertEqual(1, 1)
