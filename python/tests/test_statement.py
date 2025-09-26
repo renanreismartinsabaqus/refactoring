2511dc380de876aa77346205296eefc0bebafd25
seba-villablanca <svillablanca@abaqus.cl>
2025-09-26 11:34:43 -0300

    Add regression test
    Rregression test to ensuere he original stament function outpu remains the same

--- python/tests/test_statement.py
+++ python/tests/test_statement.py
@@ -1,10 +1,22 @@
 import unittest
-import statements
+from statements import statement, statement_html
+import json
+
+
 
 class TestStatement(unittest.TestCase):
-    def test_asset_must_have_positive_quantity(self):
-        """
-        Test statement
-        """
-        
-        self.assertEqual(1, 1)
+    def test_regression(self):
+        invoice = json.load(open("tests/invoice.json", 'r'))
+
+        plays = json.load(open("tests/plays.json", 'r'))
+
+        result = statement(invoice, plays)
+
+        exepcted = """Statement for BigCo
+ Hamlet: $650.00 (55 seats)
+ As You Like It: $580.00 (35 seats)
+ Othello: $500.00 (40 seats)
+Amount owed is $1,730.00
+You earned 47 credits
+"""
+        self.assertEqual(result, exepcted)
